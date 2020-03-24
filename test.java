302b37e6b52011227b32bf4e1fb945e7623fb6fa
coding <coding@coding.net>
2020-03-24 10:58:36 +0000

    由 Coding WebIDE 更新，https://ide.coding.net

--- test.java
+++ test.java
@@ -3,49 +3,50 @@ package solution106;
 import com.sun.xml.internal.ws.api.pipe.Tube;
 
 class Solution {
-    // public boolean calstr(int a[],int b[],string s){
-    //     // a和b分别为左右两边的选取情况，s为字符串
-    //     string A="";
-    //     string B="";
-    //     for(int i=0;i<a.length;i++){
-    //         A+=a[i];
-    //         B+=b[a.length-i];
-    //         // 获得a和b
-    //     }
-    //     if(A==B){
-    //         return true;
-    //     }
-    //     return false;
-    // }
-    public long solution(int n, String s) {
-        for(;n>0;n--){
-            result=n*result;
+    public boolean calstr(int a[],int b[],String s){
+        // a和b分别为左右两边的选取情况，s为字符串
+        String A="";
+        String B="";
+        for(int i=0;i<a.length;i++){
+            A+=s.charAt(a[i]);
+            B=B+s.charAt(b[a.length-i-1]);
+            // B+=b[i];
+            // 获得a和b
         }
-        return result*2;
-
+        if(A.equals(B)){
+            return true;
+        }
+        return false;
+    }
+    public long solution(int n, String s) {
         int N=2*n;
         int result=0,k=0,p=0;
-        int a[]=new int [4];
-        int b[]=new int [4];
+        int a[]=new int [n];
+        int b[]=new int [n];
         for(int i=0;i<(1<<N);i++){
-            int num=0;
             k=p=0;
             for(int j=0;j<N;j++){
                 if((i&(1<<j))!=0){
-                    num++;
+                    if(k==n){
+                        break;
+                    }
                     a[k++]=j;
+
                 }
-                b[p++]=j;
+                else{
+                    if(p==n){
+                        break;
+                    }
+                    b[p++]=j;
+                    
+                }
             }
-            if(num==n){
-                // if(calstr(a,b,s)){
-                //     result++;
-                // }
+            if(k==n&&p==n){
+                if(calstr(a,b,s)){
+                    result++;
+                }
             }
         }
         return result;
-
-
-
     }
 }
